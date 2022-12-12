c2bc3d6ba5c66ee87d986f1ec0ca370150ace753
MohanaSupraja <102228446+MohanaSupraja@users.noreply.github.com>
2022-12-12 11:43:40 +0530

    Update js1.js

--- library/js1.js
+++ library/js1.js
@@ -16,18 +16,3 @@ search_input.addEventListener("keyup", function(e){
   
 });
 
-search_input.addEventListener("keyup", function(e){
-  var span_items = document.querySelectorAll(".table_body .phone span");
-  var table_body = document.querySelector(".table_body ul");
-  var search_item = e.target.value.toLowerCase();
- 
- span_items.forEach(function(item){
-   if(item.textContent.toLowerCase().indexOf(search_item) != -1){
-      item.closest("li").style.display = "block";
-   }
-   else{
-     item.closest("li").style.display = "none";
-     }
- })
-  
-});
